0c38487de2ac8e4f2d232643119214ce174da58d
Robert Udrea <robert.udrea@cst.ro>
2024-01-13 15:26:05 +0200

    need help

--- Proiect3D/11_01_umbra.cpp
+++ Proiect3D/11_01_umbra.cpp
@@ -21,6 +21,7 @@
 #include <math.h>			//	Biblioteca pentru calcule matematice;
 #include <GL/glew.h>        //  Definește prototipurile functiilor OpenGL si constantele necesare pentru programarea OpenGL moderna; 
 #include <GL/freeglut.h>    //	Include functii pentru: 
+#include <stb/stb_image.h>
 							//	- gestionarea ferestrelor si evenimentelor de tastatura si mouse, 
 							//  - desenarea de primitive grafice precum dreptunghiuri, cercuri sau linii, 
 							//  - crearea de meniuri si submeniuri;
@@ -29,6 +30,42 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtx/transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+#include <string>
+
+float skyboxVertices[] =
+{
+	//   Coordinates
+	-1.0f, -1.0f,  1.0f,//        7--------6
+	 1.0f, -1.0f,  1.0f,//       /|       /|
+	 1.0f, -1.0f, -1.0f,//      4--------5 |
+	-1.0f, -1.0f, -1.0f,//      | |      | |
+	-1.0f,  1.0f,  1.0f,//      | 3------|-2
+	 1.0f,  1.0f,  1.0f,//      |/       |/
+	 1.0f,  1.0f, -1.0f,//      0--------1
+	-1.0f,  1.0f, -1.0f
+};
+
+unsigned int skyboxIndices[] =
+{
+	// Right
+	1, 2, 6,
+	6, 5, 1,
+	// Left
+	0, 4, 7,
+	7, 3, 0,
+	// Top
+	4, 5, 6,
+	6, 7, 4,
+	// Bottom
+	0, 3, 2,
+	2, 1, 0,
+	// Back
+	0, 1, 5,
+	5, 4, 0,
+	// Front
+	3, 7, 6,
+	6, 2, 3
+};
 
 
 //  Identificatorii obiectelor de tip OpenGL; 
@@ -38,6 +75,7 @@ GLuint
 	EboId,
 	ColorBufferId,
 	ProgramId,
+	ProgramId2,
 	myMatrixLocation,
 	matrUmbraLocation,
 	viewLocation,
@@ -83,6 +121,8 @@ float radius = 200;
 int vertex, index_aux;
 int startVertex = 270;
 int startIndex = 68;
+
+Shader skyboxShader("skybox.vert", "skybox.frag");
 
 void processNormalKeys(unsigned char key, int x, int y)
 {
@@ -276,6 +316,79 @@ void CreateVBO(void)
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 10 * sizeof(GLfloat), (GLvoid*)(7 * sizeof(GLfloat)));
 }
+
+unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+unsigned int cubemapTexture;
+
+void CreateSkyboxVBO(void) {
+	glGenVertexArrays(1, &skyboxVAO);
+	glGenBuffers(1, &skyboxVBO);
+	glGenBuffers(1, &skyboxEBO);
+	glBindVertexArray(skyboxVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// All the faces of the cubemap (make sure they are in this exact order)
+	std::string facesCubemap[6] =
+	{
+		"./right.jpg",
+		"./left.jpg",
+		"./top.jpg",
+		"./bottom.jpg",
+		"./front.jpg",
+		"./back.jpg"
+	};
+
+	// Creates the cubemap texture object
+	
+	glGenTextures(1, &cubemapTexture);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	// These are very important to prevent seams
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	// This might help with seams on some systems
+	//glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+	// Cycles through all the textures and attaches them to the cubemap object
+	for (unsigned int i = 0; i < 6; i++)
+	{
+		int width, height, nrChannels;
+		unsigned char* data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			stbi_set_flip_vertically_on_load(false);
+			glTexImage2D
+			(
+				GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0,
+				GL_RGB,
+				width,
+				height,
+				0,
+				GL_RGB,
+				GL_UNSIGNED_BYTE,
+				data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			//std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
+};
+
 void DestroyVBO(void)
 {
 	glDisableVertexAttribArray(2);
@@ -292,6 +405,9 @@ void CreateShaders(void)
 {
 	ProgramId = LoadShaders("11_01_Shader.vert", "11_01_Shader.frag");
 	glUseProgram(ProgramId);
+
+	ProgramId2 = LoadShaders("skybox.vert", "skybox.frag");
+	glUseProgram(ProgramId2);
 }
 
 void DestroyShaders(void)
@@ -305,6 +421,7 @@ void Initialize(void)
 	matrRot = glm::rotate(glm::mat4(1.0f), PI / 8, glm::vec3(0.0, 0.0, 1.0));
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
 	CreateVBO();
+	CreateSkyboxVBO();
 	CreateShaders();
 	// locatii pentru shader-e
 	myMatrixLocation = glGetUniformLocation(ProgramId, "myMatrix");
@@ -401,7 +518,7 @@ void RenderFunction(void)
 	glLineWidth(5.0f);
 	glDrawElements(GL_LINES, 8, GL_UNSIGNED_SHORT, (void*)(60 * sizeof(GLushort)));
 
-	//desenare umbra balon
+	//desenare umbra baloncubemapTexture
 	myMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 0.f, 525.0f));
 	glUniformMatrix4fv(myMatrixLocation, 1, GL_FALSE, &myMatrix[0][0]);
 	for (int patr = 0; patr < (NR_PARR + 1) * NR_MERID; patr++)
@@ -413,6 +530,31 @@ void RenderFunction(void)
 				GL_UNSIGNED_SHORT,
 				(void*)(((2 * (NR_PARR + 1) * (NR_MERID)+4 * patr) + 68) * sizeof(GLushort)));
 	}
+
+
+	// Since the cubemap will always have a depth of 1.0, we need that equal sign so it doesn't get discarded
+	glDepthFunc(GL_LEQUAL);
+
+	skyboxShader.Activate();
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
+	// We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
+	// The last row and column affect the translation of the skybox (which we don't want to affect)
+	view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
+	projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+	glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+	// Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
+	// where an object is present (a depth of 1.0f will always fail against any object's depth value)
+	glBindVertexArray(skyboxVAO);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, );
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	// Switch back to the normal depth function
+	glDepthFunc(GL_LESS);
 
 	glutSwapBuffers();
 	glFlush();
