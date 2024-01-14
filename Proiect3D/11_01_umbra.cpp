74ead291f36fc95c75983a84493b9240792607a0
Robert Udrea <robert.udrea@cst.ro>
2024-01-14 20:30:23 +0200

    aaaaaaaaaaa

--- Proiect3D/11_01_umbra.cpp
+++ Proiect3D/11_01_umbra.cpp
@@ -21,7 +21,7 @@
 #include <math.h>			//	Biblioteca pentru calcule matematice;
 #include <GL/glew.h>        //  Definește prototipurile functiilor OpenGL si constantele necesare pentru programarea OpenGL moderna; 
 #include <GL/freeglut.h>    //	Include functii pentru: 
-#include <stb/stb_image.h>
+#include "stb_image.h"
 							//	- gestionarea ferestrelor si evenimentelor de tastatura si mouse, 
 							//  - desenarea de primitive grafice precum dreptunghiuri, cercuri sau linii, 
 							//  - crearea de meniuri si submeniuri;
@@ -122,7 +122,6 @@ int vertex, index_aux;
 int startVertex = 270;
 int startIndex = 68;
 
-Shader skyboxShader("skybox.vert", "skybox.frag");
 
 void processNormalKeys(unsigned char key, int x, int y)
 {
@@ -366,7 +365,7 @@ void CreateSkyboxVBO(void) {
 		unsigned char* data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
 		if (data)
 		{
-			stbi_set_flip_vertically_on_load(false);
+			//stbi_set_flip_vertically_on_load(false);
 			glTexImage2D
 			(
 				GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
@@ -533,23 +532,38 @@ void RenderFunction(void)
 
 
 	// Since the cubemap will always have a depth of 1.0, we need that equal sign so it doesn't get discarded
-	glDepthFunc(GL_LEQUAL);
+	//glDepthFunc(GL_LEQUAL);
 
-	skyboxShader.Activate();
-	glm::mat4 view = glm::mat4(1.0f);
-	glm::mat4 projection = glm::mat4(1.0f);
+	// Draw skybox
+	glUseProgram(ProgramId2);
+	glDepthFunc(GL_LEQUAL); // Change depth function so depth test passes when values are equal to depth buffer's content
+	view = glm::mat4(glm::mat3(glm::lookAt(Obs, PctRef, Vert)));// Remove translation part of the view matrix
+	projection = glm::perspective(glm::radians(fov), (GLfloat)width / (GLfloat)height, znear, zfar);
+	glm::mat4 viewProjection = projection * view;
+	glUniformMatrix4fv(glGetUniformLocation(ProgramId2, "viewProjection"), 1, GL_FALSE, glm::value_ptr(viewProjection));
+	// Draw skybox cube
+	glBindVertexArray(skyboxVAO);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+	glDepthFunc(GL_LESS); // Reset depth function
+	glUseProgram(ProgramId);
+
+	//skyboxShader.Activate();
+	//glm::mat4 view = glm::mat4(1.0f);
+	//glm::mat4 projection = glm::mat4(1.0f);
 	// We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
 	// The last row and column affect the translation of the skybox (which we don't want to affect)
-	view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
-	projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-	glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
-	glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+	//view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
+	//projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+	//glUniformMatrix4fv(glGetUniformLocation(myMatrixLocation, "view"), 1, GL_FALSE, glm::value_ptr(view));
+	//glUniformMatrix4fv(glGetUniformLocation(myMatrixLocation, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
 	// Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
 	// where an object is present (a depth of 1.0f will always fail against any object's depth value)
 	glBindVertexArray(skyboxVAO);
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, );
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
 	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 
